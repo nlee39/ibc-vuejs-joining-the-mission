52cd16fc60dbddd00401114b531f744b09300e62
hechengwang <hecheng.wang@gmail.com>
2017-07-16 18:46:09 -0500

    added slides up to and including "The God Angle".

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ Vue.use(VueMaterial);
 var ibcJoiningTheMission = new Vue({
     el: '#ibc-joining-the-mission',
     data: {
-        page: 'The Good News'
+        page: 'Welcome'
     },
     methods: {
         setPage: function(page) {
