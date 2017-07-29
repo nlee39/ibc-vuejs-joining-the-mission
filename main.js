83a50f642baae45218403c437b9f76a357e859df
hechengwang <hecheng.wang@gmail.com>
2017-07-29 17:25:39 -0500

    added mobile_sidenav, desktop_sidenav
    padded current_page
    added scrollTop
    
    added sidenav to css

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var desktopSideNav = document.getElementById('desktop_sidenav');
+var desktopSideNavClone = desktopSideNav.cloneNode(true);
+desktopSideNavClone.classList.remove('desktop_sidenav');
+
+var mobileSideNav = document.getElementById('mobile_sidenav');
+mobileSideNav.appendChild(desktopSideNavClone);
+
 Vue.use(VueMaterial);
 
 var ibcJoiningTheMission = new Vue({
@@ -8,6 +15,15 @@ var ibcJoiningTheMission = new Vue({
     methods: {
         setPage: function(page) {
             this.page = page;
+            document.body.scrollTop = 0;
+            if (this.$refs.mobile_sidenav) {
+                this.$refs.mobile_sidenav.close()
+            }
+        },
+
+        openSideNav: function() {
+            this.$refs.mobile_sidenav.open()
         }
+
     }
 })
